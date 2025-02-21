55ee5e59ede1e8df1f662686819205b5d2957a0f
khangnguyenspu <khangnguyen@spu.edu>
2025-02-21 14:48:18 -0800

    Update FinalSchema.sql

--- Database/FinalSchema.sql
+++ Database/FinalSchema.sql
@@ -1,51 +1,48 @@
 --
--- File generated with SQLiteStudio v3.4.16 on Wed Feb 19 12:48:49 2025
+-- File generated with SQLiteStudio v3.4.16 on Fri Feb 21 14:46:07 2025
 --
 -- Text encoding used: System
 --
 PRAGMA foreign_keys = off;
 BEGIN TRANSACTION;
 
--- Table: Project
-DROP TABLE IF EXISTS Project;
+-- Table: projectID
+DROP TABLE IF EXISTS projectID;
 
-CREATE TABLE IF NOT EXISTS Project (
-    Project_id  INTEGER NOT NULL
-                        PRIMARY KEY AUTOINCREMENT,
+CREATE TABLE IF NOT EXISTS projectID (
+    Project_ID  INTEGER PRIMARY KEY AUTOINCREMENT,
     Name        TEXT    NOT NULL,
     Description TEXT,
-    Status      TEXT    NOT NULL,
-    Progress    INTEGER NOT NULL
+    status      TEXT    NOT NULL
+                        REFERENCES Status (status) 
 );
-
 
 -- Table: Reminder
 DROP TABLE IF EXISTS Reminder;
 
 CREATE TABLE IF NOT EXISTS Reminder (
-    Reminder_id   INTEGER PRIMARY KEY AUTOINCREMENT
-                          NOT NULL,
+    Reminder_id   INTEGER PRIMARY KEY AUTOINCREMENT,
     Title         TEXT    NOT NULL,
-    Due_date      TEXT    NOT NULL,
+    Due_Date      TEXT    NOT NULL,
+    Priority      INTEGER CHECK (Priority BETWEEN 1 AND 5),
     Reminder_date TEXT    NOT NULL,
-    Priority      INTEGER NOT NULL,
-    Project_id    INTEGER REFERENCES Project (Project_id),
-    Task_id       INTEGER REFERENCES Task (Task_id) 
+    Project_ID    INTEGER NOT NULL
+                          REFERENCES projectID (Project_ID),
+    Task_ID       INTEGER REFERENCES Task (Task_ID) 
+                          NOT NULL
 );
-
 
 -- Table: Task
 DROP TABLE IF EXISTS Task;
 
 CREATE TABLE IF NOT EXISTS Task (
-    Task_id            INTEGER PRIMARY KEY AUTOINCREMENT
+    Task_ID            INTEGER PRIMARY KEY
                                NOT NULL,
-    Is_recurring       INTEGER NOT NULL,
-    Recurring_interval INTEGER NOT NULL,
-    Title              TEXT    NOT NULL,
-    Project_id         INTEGER REFERENCES Project (Project_id) 
+    is_recurring       INTEGER NOT NULL,
+    recurring_interval INTEGER NOT NULL,
+    category           TEXT,
+    Project_ID         INTEGER REFERENCES projectID (Project_ID) 
+                               NOT NULL
 );
-
-
 COMMIT TRANSACTION;
 PRAGMA foreign_keys = on;
